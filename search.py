2dc575110f4816c57b992bde3d2fb1e6319cc690
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2023-03-24 17:32:39 +1100

    Fix small typo in L>U check

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -308,7 +308,7 @@ def bidirectionalAStarEnhanced(problem, heuristic=nullHeuristic, backwardsHeuris
                 del backwardActions[-1]
                 plan = forwardActions + [costForward[state][1]] + backwardActions
 
-        if L >= U and initialGoal:
+        if L >= U:
             return plan
 
         if searchDir.dir == 'F':
